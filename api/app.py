805b88c5878b4a937151622cc54d00f6d68103c1
Daniel Lwetabe <dannylwe11@gmail.com>
2019-11-20 17:10:45 +0300

    db created

--- api/app.py
+++ api/app.py
@@ -2,7 +2,42 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
+# import database
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+#databse
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#db init
+db = SQLAlchemy(app)
+#init marshmallow
+ma = Marshmallow(app)
+
+#product Class/Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+
+#product schema
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'qty') 
+
+#Init schema
+product_schema = ProductSchema() 
+products_schema = ProductSchema(many=True) 
 
 
+@app.route('/health', methods=['GET'])
+def healthCheck():
+    return jsonify({"mesaage": "server is running"})
